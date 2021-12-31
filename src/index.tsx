bb2757f97e094f911e663aede8f3c025b3e8566a
Pranav Teegavarapu <pranavnt@outlook.com>
2021-12-30 23:37:39 -0800

    some types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,33 @@
 import * as React from 'react';
 
-export const Snippet = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export const Snippet = (props: SnippetProps) => {
+  let [code, setCode] = React.useState(props.code[0] || null);
+  let languages: string[] = props.code.map(a => a.language);
+
+  return (
+    <>
+      <option>
+        {languages.map((language, index) => (
+          <option key={index}>{language}</option>
+        ))}
+      </option>
+      <span
+        onClick={() => {
+          let code = props.code.filter(a => a.language === props.language);
+        }}
+      >
+        📝 Copy
+      </span>
+    </>
+  );
 };
+
+interface SnippetProps {
+  theme: string;
+  code: SnippetCode[];
+}
+
+interface SnippetCode {
+  code: string;
+  language: string;
+}
