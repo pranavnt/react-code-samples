b1d749cde338b51d88672c9055f0550e01b7cce0
Pranav Teegavarapu <pranavnt@outlook.com>
2021-12-31 00:59:39 -0800

    dropdown value binded to state

--- example/index.tsx
+++ example/index.tsx
@@ -19,6 +19,12 @@ const App = () => {
             `,
             language: 'rust',
           },
+          {
+            code: `
+              fmt.Println("Hello, world!")
+            `,
+            language: 'go',
+          },
         ]}
         theme="nord"
       />
