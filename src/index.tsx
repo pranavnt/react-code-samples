5651658bbb846ab56d9fcbeacd8f546c0b16130d
Pranav Teegavarapu <pranavnt@outlook.com>
2021-12-31 01:40:33 -0800

    syntax highlighting works :D

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react';
+//@ts-ignore
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
 
 export const Snippet = (props: SnippetProps) => {
-  //@ts-ignore (unused var for time being)
   let [code, setCode] = React.useState(props.code[0] || null);
   let languages: string[] = props.code.map(a => a.language);
 
   return (
     <>
       <div>
-        <select>
+        <select
+          onChange={e => {
+            let newCode = props.code.find(
+              a => a.language === e.currentTarget.value
+            ) as SnippetCode;
+            setCode(newCode);
+            let el = document.getElementById('code');
+            if (el) {
+              el.innerHTML = hljs.highlight(newCode.code, {
+                language: newCode.language,
+              }).value;
+            }
+          }}
+        >
           {languages.map((language, index) => (
             <option key={index}>{language}</option>
           ))}
         </select>
+        {
+          // show hljs themes select
+        }
         <span
           onClick={() => {
             navigator.clipboard.writeText(code.code);
@@ -26,7 +44,7 @@ export const Snippet = (props: SnippetProps) => {
         </span>
       </div>
       <div>
-        <code>{code.code}</code>
+        <div id="code" />
       </div>
     </>
   );
